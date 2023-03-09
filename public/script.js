00779a757246eab651788b0a12ffdfedca9a5543
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-09 07:30:25 +0100

    improve: added unique welcome message to users

--- public/script.js
+++ public/script.js
@@ -1,5 +1,14 @@
 const socket = io("ws://localhost:5000");
 
-socket.on("connected", function (msg) {
-  console.log("Customer connected");
+socket.on("welcome", function (msg) {
+  console.log(msg);
+  const welcomeMessageElement = document.getElementById('welcome')
+  const p = document.createElement('p')
+  p.textContent = `${msg}`
+  welcomeMessageElement.append(p)
 });
+
+
+orderButton.addEventListener('click', function(e){
+    socket.emit()
+})
