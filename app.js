abb356e762cf06572e936679dc4fe9b4f5b05015
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-09 07:27:57 +0100

    new: added user session

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { Server } = require('socket.io');
 const http = require('http');
+const cookieParser = require('cookie-parser') 
+const { v4: uuidv4} = require('uuid')
+const sessionMiddleware = require('./middlewares/sessionMiddleware')
 const CONFIG = require('./config/config')
 require('./config/db')( CONFIG.DB )
 
@@ -9,16 +12,47 @@ const server = http.createServer(app);
 const io = new Server(server)
 const PORT = CONFIG.PORT 
 
+
+
 /*MIDDLEWARES*/
-app.use(express.static('public'))
+app.use(sessionMiddleware)
+app.use( express.static('public') )
+app.use(cookieParser())
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public' + '/chatbot.html')
 })
 
-io.on("connection", (socket) => {
+
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, {}, next);
+  
+});
+
+io.on("connection", async(socket) => {
+    const session = socket.request.session;
+    let userID = session.userId
+
+    if ( !userID ){
+        userID = uuidv4();
+
+        socket.request.session.userId = userID 
+        session.save((err) => {
+          if (err) {
+            console.error("Error saving session:", err);
+          } else {
+            console.log("Saved user ID to session:", userID);
+          }
+        });
+        console.log("New user joined!");
+        socket.emit("welcome", "Welcome to food order chatbot, How may i help you today?");
+    }
+    else{
+        console.log('old member')
+        socket.emit("welcome", "Welcome back to food order chatbot, How may i help you today?");
+    }
     
     console.log("client connected", socket.id)
-    socket.emit("connected", "connected to backend server");
+    
 })
 
 server.listen(PORT, ()=>{
