0990fec8c1796b1ed65add2b8b56d27302d77272
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-11 00:24:15 +0100

    new: added helper functions

---
+++ helpers.js
@@ -0,0 +1,11 @@
+const Menu = [
+    {key: "1", value: "Jollof Rice", price: "$100"},
+    {key: "2", value: "Pizza", price: "$50"},
+    {key: "3", value: "Sharwama", price: "$20"},
+    {key: "4", value: "Red Velvet Cake", price: "$10"},
+    { key: "5", value: "Amala", price: "$5"}
+
+]
+
+
+module.exports={ Menu };
