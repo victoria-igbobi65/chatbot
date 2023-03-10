b57b039438bf1c13a31cdce19ffa07a919553e3b
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-11 00:25:41 +0100

    improve: added new environment variables

--- config/config.js
+++ config/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 8000;
 const DB = process.env.DB;
+const SECRET = process.env.SECRET;
 
 module.exports={
     PORT,
-    DB
+    DB,
+    SECRET
 }
