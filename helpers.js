00c1ea6d2cf11e5dc10db10671c290faafd39b91
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-14 23:37:33 +0100

    improve: added img url to menu

--- helpers.js
+++ helpers.js
@@ -1,9 +1,9 @@
 const Menu = [
-    {key: "1", value: "Jollof Rice", price: "$100"},
-    {key: "2", value: "Pizza", price: "$50"},
-    {key: "3", value: "Sharwama", price: "$20"},
-    {key: "4", value: "Red Velvet Cake", price: "$10"},
-    { key: "5", value: "Amala", price: "$5"}
+    {key: "1", value: "Jollof Rice", price: "$100", img: "./img/jollof.jpg"},
+    {key: "2", value: "Pizza", price: "$50", img: "./img/pizza1.jpg"},
+    {key: "3", value: "Ice Cream", price: "$20", img: "./img/icecream.jpg"},
+    {key: "4", value: "Muffin", price: "$10", img: "./img/muffin.jpg"},
+    { key: "5", value: "Burger", price: "$5", img: "./img/burger.png"}
 
 ]
 
