c203ab14d2726437c1af8598b31b223a88301366
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-14 23:37:56 +0100

    improve: modified file

--- middlewares/connection.js
+++ middlewares/connection.js
@@ -18,12 +18,11 @@ const connectionMiddleware = (socket) => {
             
         })
         console.log('New user joined!')
-        socket.emit('welcome', 'Welcome to food order chatbot, How may i help you today?'
-        )
+        socket.emit('welcome', 'Hey👋🏻!, I am the restaurant\'s automated assistant. How can i help you today?')
     } else {
 
         console.log('old member')
-        socket.emit( 'welcome', 'Welcome back to food order chatbot, How may i help you today?' )
+        socket.emit( 'welcome', 'Welcome back 👋🏻, I am the restaurant\'s automated assistant. How can i help you today?' )
 
     }
 
@@ -36,7 +35,7 @@ const connectionMiddleware = (socket) => {
         if ( data.action === '1' ) {
 
             socket.emit( 'user_request', 'place order' )
-            socket.emit( 'bot_response', { message: 'please check below for menu 👇', menu: Menu })
+            socket.emit( 'bot_response', { message: 'Menu List', menu: Menu })
         } 
         /* Gets executed when a  '88' event occurs */
         else if ( data.action === '88' ) {
@@ -49,17 +48,16 @@ const connectionMiddleware = (socket) => {
 
             const currentOrder = await getCurrentOrder({ userid: userID })
             const status = 'cancelled'
-            socket.emit( 'user_request', `Cancel my current Order: ${currentOrder.itemname}`
-            )
+            socket.emit('user_request', `Cancel my current Order `)
 
             if ( currentOrder && currentOrder.status === 'pending' ) {
 
                 await updateStatus(currentOrder._id, { $set: { status } })
-                socket.emit( 'cancel_response', `Your current order ${currentOrder.itemname} has been cancelled` )
+                socket.emit( 'cancel_response', { message: `Your current order ${currentOrder.itemname} has been cancelled`, object: currentOrder } )
 
             } else {
 
-                socket.emit( 'cancel_response', "You haven't placed any order yet")
+                socket.emit( 'completed_response', "You haven't placed any order yet")
             }
             
         } 
@@ -68,16 +66,16 @@ const connectionMiddleware = (socket) => {
 
             const currentOrder = await getCurrentOrder({ userid: userID })
             const status = 'completed'
-            socket.emit( 'user_request', `Checkout my current Order: ${ currentOrder.itemname }` )
+            socket.emit( 'user_request', ` Checkout my current Order ` )
 
             if ( currentOrder && currentOrder.status === 'pending' ) {
 
                 await updateStatus( currentOrder._id, { $set: { status } })
-                socket.emit( 'cancel_response', `Your current order ${ currentOrder.itemname } has been ordered!` )
+                socket.emit( 'cancel_response', { message: `Your current order ${ currentOrder.itemname } has been ordered!`, object: currentOrder } )
 
             } else {
 
-                socket.emit( 'cancel_response', "You haven't placed any order yet")
+                socket.emit( 'completed_response', "You haven't placed any order yet")
 
             }
         } 
@@ -89,18 +87,18 @@ const connectionMiddleware = (socket) => {
 
             if ( currentOrder && currentOrder.status === 'pending' ) {
 
-                socket.emit( 'cancel_response', `Your Current Order: ${ currentOrder.itemname }` )
+                socket.emit( 'cancel_response', { message: `Your Current Order: ${ currentOrder.itemname }`, object: currentOrder } )
 
             } else {
 
-                socket.emit( 'menu_response', "You haven't placed any order yet" )
+                socket.emit( 'completed_response', "You haven't placed any order yet" )
 
             }
         } 
         /* Gets executed when a '98' event occurs */
         else if (data.action === '98') {
 
-            const orders = await allOrder({ userid: userID })
+            const orders = await allOrder({ "status": { $in: ['completed', 'cancelled'] }, userid: userID })
             socket.emit('user_request', 'See my Order History')
             socket.emit('history', orders)
 
@@ -114,10 +112,11 @@ const connectionMiddleware = (socket) => {
         await newOrder({
             userid: userID,
             itemid: data.key,
+            img: data.img,
             itemname: data.value,
         })
         socket.emit( 'user_request', `My Current Order: ${ data.value }` )
-        socket.emit( 'menu_response', `Order: ${ data.value } Price: ${ order.price }` )
+        socket.emit( 'menu_response', { message: `${ data.value } ${ order.price }`, object: order } )
     })
 }
 
