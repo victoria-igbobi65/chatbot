14f3661ec30251f8ec06e7eb7881ef968bc785d2
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-15 20:48:03 +0100

    improve: added meals to menu

--- helpers.js
+++ helpers.js
@@ -3,7 +3,12 @@ const Menu = [
     {key: "2", value: "Pizza", price: "$50", img: "./img/pizza1.jpg"},
     {key: "3", value: "Ice Cream", price: "$20", img: "./img/icecream.jpg"},
     {key: "4", value: "Muffin", price: "$10", img: "./img/muffin.jpg"},
-    { key: "5", value: "Burger", price: "$5", img: "./img/burger.png"}
+    { key: "5", value: "Burger", price: "$5", img: "./img/burger.png"},
+    { key: "6", value: "ButterMilk Pancake", price: "$20", img: "./img/pancake.webp"},
+    { key: "7", value: "Chicken & Waffles", price: "$5", img: "./img/waffles.webp"},
+    { key: "8", value: "Fruit Smoothies", price: "$5", img: "./img/smoothies.webp"},
+    { key: "9", value: "Croissants", price: "$15", img: "./img/croissant.webp"},
+    { key: "10", value: "Hot Dog", price: "$35", img: "./img/hotDog.webp"},
 
 ]
 
